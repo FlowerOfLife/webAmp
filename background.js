205f755583d4d2af1caa7f7132cdcdb21dde3d45
kaparanoid <sasha.tsipin@gmail.com>
2014-05-10 19:55:38 +0300

    added running text

--- background.js
+++ background.js
@@ -1,8 +1,13 @@
 chrome.app.runtime.onLaunched.addListener(function () {
-	chrome.app.window.create('index.html', {
-		'bounds': {
-			'width': 500,
-			'height': 600
-		}
-	});
+    chrome.app.window.create('index.html', {
+        'bounds': {
+            "width": 500,
+            "height": 500
+
+        },
+        minWidth: 300,
+        minHeight: 200,
+        maxWidth: 700,
+        maxHeight: 900,
+    });
 });
